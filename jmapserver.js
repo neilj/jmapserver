df69ef7de0007669c73e7510e1204011b6942b8a
Neil Jenkins <neil@nmjenkins.com>
2021-02-12 12:08:49 +1100

    Add /changes implementation

--- jmapserver.js
+++ jmapserver.js
@@ -1,9 +1,7 @@
 import { Database, iterate, promisify as _ } from './Database.js';
 
 const types = {
-    Email: {
-
-    },
+    Email: {},
     Thread: {
         // ontxn: async (objects, transaction)  => ())}
     },
@@ -11,9 +9,10 @@ const types = {
 };
 
 class JMAPServer {
-    constructor(userId) {
+    constructor(accountId) {
+        this.accountId = accountId;
         this.db = new Database({
-            name: `JMAP-${userId}`,
+            name: `JMAP-${accountId}`,
             version: 1,
             setup(db /*, newVersion, oldVersion*/) {
                 const metaStore = db.createObjectStore('Meta', {
@@ -34,40 +33,105 @@ class JMAPServer {
         });
     }
 
-    addRecords(/** @type string */ typeName, /** @type any[] */records) {
-        this.db.transaction(
-            ['Meta', typeName],
-            'readwrite',
-            async (/** @type IDBTransaction */ transaction) => {
-                const metaStore = transaction.objectStore('Meta');
-                const typeStore = transaction.objectStore(typeName);
-                const meta = await _(metaStore.get(typeName));
-                const existing = await Promise.all(
-                    records.map((record) => _(typeStore.get(record.id))),
-                );
-                let modseq = meta.highestModSeq;
-                records.forEach((record, i) => {
-                    if (!record.id) {
-                        throw new Error('Must have an id!')
-                    }
-                    modseq += 1;
-                    const prevValue = existing[i];
-                    typeStore.put({
-                        ...prevValue,
-                        ...record,
-                        _createdModSeq: prevValue
-                            ? prevValue._createdModSeq
-                            : modseq,
-                        _updatedModSeq: modseq,
-                        _deleted: null,
-                    });
+    addRecords(/** @type string */ typeName, /** @type any[] */ records) {
+        this.db.transaction(['Meta', typeName], 'readwrite', async (
+            /** @type IDBTransaction */ transaction,
+        ) => {
+            const metaStore = transaction.objectStore('Meta');
+            const typeStore = transaction.objectStore(typeName);
+            const meta = await _(metaStore.get(typeName));
+            const existing = await Promise.all(
+                records.map((record) => _(typeStore.get(record.id))),
+            );
+            let modseq = meta.highestModSeq;
+            records.forEach((record, i) => {
+                if (!record.id) {
+                    throw new Error('Must have an id!');
+                }
+                modseq += 1;
+                const prevValue = existing[i];
+                typeStore.put({
+                    ...prevValue,
+                    ...record,
+                    _createdModSeq: prevValue
+                        ? prevValue._createdModSeq
+                        : modseq,
+                    _updatedModSeq: modseq,
+                    _deleted: null,
                 });
-                metaStore.put({
-                    ...meta,
-                    highestModSeq: modseq,
-                });
-            },
-        );
+            });
+            metaStore.put({
+                ...meta,
+                highestModSeq: modseq,
+            });
+        });
+    }
+
+    async changes(typeName, args) {
+        const sinceState = args.sinceState;
+        let maxChanges = args.maxChanges || 0;
+        if (!(maxChanges > 0 && maxChanges <= 1024)) {
+            maxChanges = 1024;
+        }
+        if (typeof sinceState !== 'string') {
+            // error
+            return;
+        }
+        const sinceModSeq = parseInt(sinceState, 10);
+        if (isNaN(sinceModSeq)) {
+            // error
+            return;
+        }
+        // Do stuff
+        let upToModSeq = sinceModSeq;
+        let hasMoreChanges = false;
+        const created = [];
+        const updated = [];
+        const destroyed = [];
+        await this.db.transaction(['Meta', typeName], 'readonly', async (
+            /** @type IDBTransaction */ transaction,
+        ) => {
+            const metaStore = transaction.objectStore('Meta');
+            const typeStore = transaction.objectStore(typeName);
+            const meta = await _(metaStore.get(typeName));
+            if (meta.highestModSeq === sinceModSeq) {
+                return;
+            }
+            const cursor = typeStore
+                .index('byModSeq')
+                .openCursor(IDBKeyRange.lowerBound(sinceModSeq, true), 'next');
+            let count = 0;
+            for await (const result of iterate(cursor)) {
+                if (count === maxChanges) {
+                    hasMoreChanges = true;
+                    break;
+                }
+                const record = result.value;
+                const id = record.id;
+                const isCreated = record._createdModSeq > sinceModSeq;
+                if (record._deleted) {
+                    if (!isCreated) destroyed.push(id);
+                } else if (isCreated) {
+                    created.push(id);
+                } else {
+                    updated.push(id);
+                }
+                upToModSeq = record._updatedModSeq;
+                count += 1;
+            }
+            if (!hasMoreChanges) {
+                upToModSeq = meta.highestModSeq;
+            }
+        });
+        return {
+            accountId: this.accountId,
+            oldState: sinceState,
+            newState: upToModSeq + '',
+            hasMoreChanges,
+            created,
+            updated,
+            destroyed,
+        };
     }
 }
 
@@ -96,7 +160,11 @@ const process = (request, session) => {
 // ---
 
 const server = new JMAPServer('foo');
-server.addRecords('Email', [{
-    id: '123',
-    subject: 'This is the subject',
-}])
+server.addRecords('Email', [
+    {
+        id: '123',
+        subject: 'This is the subject',
+    },
+]);
+
+window.server = server;
